948ae78c4e71b8c6477eb4741e36e0f517eae8ac
Jonas <JonasKrabs@gmx.de>
2017-05-09 17:11:31 +0200

    Testcommit after transfering repository ownership to michael and adding the DAMMInitializer class

---
+++ BiberDAMM/DAL/DAMMInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiberDAMM.DAL
+{
+    public class DAMMInitializer
+    {
+        //Testcommit after transfering repository ownership to Michael // Jonas
+    }
+}
