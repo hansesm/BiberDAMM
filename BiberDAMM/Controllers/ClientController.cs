d66d64d1815960e0119b3fcebc5618f9b03ace9e
leonhard.jentzsch <leonhard.jentzsch@student.uni-siegen.de>
2017-05-14 18:08:32 +0200

    ClientController ergänzt

--- BiberDAMM/Controllers/ClientController.cs
+++ BiberDAMM/Controllers/ClientController.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BiberDAMM.Models;
 
-public class ClientController : Controller {
 
-    public ActionResult Index()
+namespace BiberDAMM.Controllers
+{
+
+    public class ClientController : Controller
     {
-        return View();
+
+        // GET all: Client [JEL] [ANNAS]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //CREATE: Client [JEL] [ANNAS]
+        public ActionResult New()
+        {
+            return View();
+        }
+        //CHANGE: Client [JEL] [ANNAS]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        //GET SINGLE: Client [JEL] [ANNAS]
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        //SAVE: Client [JEL] [ANNAS]
+        public ActionResult Save()
+        {
+            return View();
+        }
+
     }
-    
 }
