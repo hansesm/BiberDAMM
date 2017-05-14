a3cb3b8c0284bd2c6a6cf312ef4e469211a92d30
Anna <anna.schwarze@outlook.de>
2017-05-14 13:44:24 +0200

    TreatmentController Angelegt

---
+++ BiberDAMM/Controllers/TreatmentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiberDAMM.Controllers
+{
+    public class TreatmentController : Controller
+    {
+        // GET all: Treatment [JEL] [ANNAS]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //CREATE: Treatment [JEL] [ANNAS]
+        public ActionResult New()
+        {
+            return View();
+        }
+        //CHANGE: Treatment [JEL] [ANNAS]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        //GET SINGLE: Treatment [JEL] [ANNAS]
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        //SAVE: Treatment [JEL] [ANNAS]
+        public ActionResult Save()
+        {
+            return View();
+        }
+    }
+}
