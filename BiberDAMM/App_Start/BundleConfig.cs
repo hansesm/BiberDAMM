14cbc68053d009a3c8ce237c00072a495e98a633
hansesm <hanses.michael@live.de>
2017-05-15 12:41:49 +0200

    Changed Bootstrap-Theme, added images folder

--- BiberDAMM/App_Start/BundleConfig.cs
+++ BiberDAMM/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace BiberDAMM
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-yeti.css",
                       "~/Content/site.css"));
         }
     }
