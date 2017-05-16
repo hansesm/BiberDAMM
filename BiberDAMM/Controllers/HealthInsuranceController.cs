96dfc042c6088f36d6669e28e4944273c2f3893c
Robin Christes <robin.christes@outlook.com>
2017-05-16 12:25:21 +0200

    Anzeige und Bearbeitung von Krankenversicherung hinzugefügt

--- BiberDAMM/Controllers/HealthInsuranceController.cs
+++ BiberDAMM/Controllers/HealthInsuranceController.cs
@@ -1,38 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BiberDAMM.DAL;
 using BiberDAMM.Models;
 
 namespace BiberDAMM.Controllers
 {
     public class HealthInsuranceController : Controller
     {
-        // GET all: HealthInsurance [JEL] [ANNAS]
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: HealthInsurances
         public ActionResult Index()
         {
-            return View();
+            return View(db.HealthInsurances.ToList());
         }
-        //CREATE: HealthInsurance [JEL] [ANNAS]
-        public ActionResult New()
+
+        // GET: HealthInsurances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthInsurance healthInsurance = db.HealthInsurances.Find(id);
+            if (healthInsurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthInsurance);
+        }
+
+        // GET: HealthInsurances/Create
+        public ActionResult Create()
         {
             return View();
         }
-        //CHANGE: HealthInsurance [JEL] [ANNAS]
-        public ActionResult Edit()
+
+        // POST: HealthInsurances/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Type")] HealthInsurance healthInsurance)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.HealthInsurances.Add(healthInsurance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(healthInsurance);
         }
-        //GET SINGLE: HealthInsurance [JEL] [ANNAS]
-        public ActionResult Detail()
+
+        // GET: HealthInsurances/Edit/5
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthInsurance healthInsurance = db.HealthInsurances.Find(id);
+            if (healthInsurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthInsurance);
         }
-        //SAVE: HealthInsurance [JEL] [ANNAS]
-        public ActionResult Save()
+
+        // POST: HealthInsurances/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Type")] HealthInsurance healthInsurance)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Entry(healthInsurance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(healthInsurance);
+        }
+
+        // GET: HealthInsurances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthInsurance healthInsurance = db.HealthInsurances.Find(id);
+            if (healthInsurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthInsurance);
+        }
+
+        // POST: HealthInsurances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HealthInsurance healthInsurance = db.HealthInsurances.Find(id);
+            db.HealthInsurances.Remove(healthInsurance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
