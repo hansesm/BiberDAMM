2fac14258131f49a26134c6082c98f2c49a68d4a
JPK <jp.kelleter@gmx.de>
2017-05-18 20:44:18 +0200

    Views für Bed hinzugefügt, Controller angepasst

--- BiberDAMM/Controllers/BedController.cs
+++ BiberDAMM/Controllers/BedController.cs
@@ -1,33 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Controller for Bed
+ * Initially created by: JEL, ANNAS
+ * Edited: Jean-PierreK
+ */
+
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using BiberDAMM.Models;
+using BiberDAMM.DAL;
+using System.Data.Entity;
 
 namespace BiberDAMM.Controllers
 {
     public class BedController : Controller
     {
-        // GET all: Bed [JEL] [ANNAS]
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //GET /Bed/
         public ActionResult Index()
         {
-            return View();
+            return View(db.Beds.ToList());
         }
-        //CREATE: Bed [JEL] [ANNAS]
+        //GET /Bed/Create to add a bed 
         public ActionResult New()
         {
             return View();
         }
-        //CHANGE: Bed [JEL] [ANNAS]
-        public ActionResult Edit()
+        // Validation token currently causing error, therefor commented out for now
+        //[HttpPost]                        
+        //[ValidateAntiForgeryToken]        
+        public ActionResult Create([Bind(Include = "Id,Model,RoomId")] Bed bed)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Beds.Add(bed);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bed);
         }
-        //GET SINGLE: Bed [JEL] [ANNAS]
-        public ActionResult Detail()
+        //Get /Bed/Edit for id
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Bed bed = db.Beds.Find(id);
+            if (bed == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bed);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Model,RoomId")] Bed bed)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bed).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bed);
+        }
+        //GET Bed/Details for Bed with id
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Bed bed = db.Beds.Find(id);
+            if (bed == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bed);
         }
         //SAVE: Bed [JEL] [ANNAS]
         public ActionResult Save()
