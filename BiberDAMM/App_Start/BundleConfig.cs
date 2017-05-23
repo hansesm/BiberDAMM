531aeef8cf1214f934136b80c6aa39ace472c6d1
hansesm <hanses.michael@live.de>
2017-05-23 13:09:22 +0200

    Bundle-Config + Render Section

--- BiberDAMM/App_Start/BundleConfig.cs
+++ BiberDAMM/App_Start/BundleConfig.cs
@@ -7,11 +7,13 @@ namespace BiberDAMM
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.validate*")
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                //"~/Scripts/jquery.validate*"));
 
             //TODO Change Modenizr-Version [HansesM]
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
