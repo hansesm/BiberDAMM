c4c3d66d3f1f38fa945609da4c945356e95ce4c3
hansesm <hanses.michael@live.de>
2017-05-23 11:47:24 +0200

    Login-Layout Changes, changed  _ViewStart

--- BiberDAMM/App_Start/BundleConfig.cs
+++ BiberDAMM/App_Start/BundleConfig.cs
@@ -14,15 +14,18 @@ namespace BiberDAMM
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            //TODO Change Modenizr-Version [HansesM]
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            //TODO Add Java-Script-Classes to Bundle to reducre HTTP Connections [HansesM]
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //TODO Add Css-Stylesheets to Bundle to reducre HTTP Connections [HansesM]
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/style.css",
                       "~/Content/fonts/font-awesome/css/font-awesome.min.css"));
