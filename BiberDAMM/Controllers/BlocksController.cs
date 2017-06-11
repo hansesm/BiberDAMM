2aba05b86bb26a494c309eeda8aa95636780dd63
hansesm <hanses.michael@live.de>
2017-06-11 14:50:17 +0200

    Blocks Create, Model changes

--- BiberDAMM/Controllers/BlocksController.cs
+++ BiberDAMM/Controllers/BlocksController.cs
@@ -1,19 +1,52 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BiberDAMM.DAL;
+using BiberDAMM.Models;
+using BiberDAMM.ViewModels;
 
 namespace BiberDAMM.Controllers
 {
     public class BlocksController : Controller
     {
+        //The Database-Context [HansesM]
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
         // GET all: Blocks [JEL] [ANNAS]
         public ActionResult Index()
         {
             return View();
         }
 
-        //CREATE: Blocks [JEL] [ANNAS]
-        public ActionResult New()
+        //CREATE: Blocks [HansesM]
+        public ActionResult Create(int id)
         {
-            return View();
+            //Gets the Stay from the given id [HansesM]
+            var stay = _db.Stays.SingleOrDefault(m => m.Id == id);
+
+            if (stay != null)
+            {
+                var newBlocks = new Blocks();
+                newBlocks.Stay = stay;
+                newBlocks.StayId = stay.Id;
+
+                //Gets a list of Doctors for the Dropdownlist [HansesM]
+                var listBedModels = _db.Beds.ToList();
+                //Builds a selectesList out of the list of doctors [HansesM]
+                var selectetlistBedModels = new List<SelectListItem>();
+                foreach (var m in listBedModels)
+                {
+                    selectetlistBedModels.Add(new SelectListItem { Text = m.Model.ToString(), Value = (m.Id.ToString()) });
+                }
+                
+                var viewModel = new BlocksCreateViewModel(newBlocks, stay, selectetlistBedModels);
+                return View(viewModel);
+            }
+            else
+            {
+                //TODO No stay found
+                return RedirectToAction("Index", "Stay");
+            }
         }
 
         //CHANGE: Blocks [JEL] [ANNAS]
@@ -32,6 +65,12 @@ namespace BiberDAMM.Controllers
         public ActionResult Save()
         {
             return View();
+        }
+
+        //Override on Dispose for security reasons. [HansesM]
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
         }
     }
 }
---
+++ BiberDAMM/ViewModels/BlocksCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using BiberDAMM.Models;
+
+namespace BiberDAMM.ViewModels
+{
+    public class BlocksCreateViewModel
+    {
+        public Stay Stay { get; set; }
+        public Blocks Blocks { get; set; }
+        public List<SelectListItem> ListBedModels { get; set; }
+
+
+        public BlocksCreateViewModel(Blocks blocks, Stay stay, List<SelectListItem> ListBedModels)
+        {
+            this.Stay = stay;
+            this.Blocks = Blocks;
+            this.ListBedModels = ListBedModels;
+        }
+
+        public BlocksCreateViewModel()
+        {
+
+        }
+    }
+}
