d971d135c4d19c898fabd54cec0951271f4261dc
hansesm <hanses.michael@live.de>
2017-06-21 18:19:15 +0200

    Comments in Room-Scheduler

---
+++ BiberDAMM/Helpers/JsonRoomSchedulerEvents.cs
@@ -0,0 +1,15 @@
+﻿//Author: [HansesM]
+
+namespace BiberDAMM.Helpers
+{
+    //Class for for displaying room-schedule in a timetable view, needed in the Room-Scheduler-View and Controllers [HansesM]
+    public class JsonRoomSchedulerEvents
+    {
+        //java-script want lowercase variables. =( 
+        public string roomName { get; set; }
+
+        public string treatmentType { get; set; }
+        public string beginDate { get; set; }
+        public string endDate { get; set; }
+    }
+}
