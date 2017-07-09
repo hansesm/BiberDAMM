c1f9f44ed989449003c60fa57da42bbba5080f5f
Jonas <JonasKrabs@gmx.de>
2017-07-09 16:21:32 +0200

    cleaned manage controller and views

--- BiberDAMM/Controllers/ManageController.cs
+++ BiberDAMM/Controllers/ManageController.cs
@@ -1,12 +1,10 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using BiberDAMM.Models;
 using BiberDAMM.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using BiberDAMM.Helpers;
 
 namespace BiberDAMM.Controllers
@@ -39,194 +37,12 @@ namespace BiberDAMM.Controllers
             private set => _userManager = value;
         }
 
-        //
-        // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess
-                    ? "Ihr Kennwort wurde geändert."
-                    : message == ManageMessageId.SetPasswordSuccess
-                        ? "Ihr Kennwort wurde festgelegt."
-                        : message == ManageMessageId.SetTwoFactorSuccess
-                            ? "Ihr Anbieter für zweistufige Authentifizierung wurde festgelegt."
-                            : message == ManageMessageId.Error
-                                ? "Fehler"
-                                : message == ManageMessageId.AddPhoneSuccess
-                                    ? "Ihre Telefonnummer wurde hinzugefügt."
-                                    : message == ManageMessageId.RemovePhoneSuccess
-                                        ? "Ihre Telefonnummer wurde entfernt."
-                                        : "";
-
-            var userId = User.Identity.GetUserId();
-            var model = new IndexViewModel
-            {
-                HasPassword = HasPassword(),
-                /* Change PrimaryKey of identity package to int //KrabsJ
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-                Logins = await UserManager.GetLoginsAsync(userId),
-                */
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(User.Identity.GetUserId<int>()),
-                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(User.Identity.GetUserId<int>()),
-                Logins = await UserManager.GetLoginsAsync(User.Identity.GetUserId<int>()),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
-            };
-            return View(model);
-        }
-
-        //
-        // POST: /Manage/RemoveLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
-        {
-            ManageMessageId? message;
-            //Change PrimaryKey of identity package to int [var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));] //KrabsJ
-            var result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId<int>(),
-                new UserLoginInfo(loginProvider, providerKey));
-            if (result.Succeeded)
-            {
-                //Change PrimaryKey of identity package to int [var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());] //KrabsJ
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-                if (user != null)
-                    await SignInManager.SignInAsync(user, false, false);
-                message = ManageMessageId.RemoveLoginSuccess;
-            }
-            else
-            {
-                message = ManageMessageId.Error;
-            }
-            return RedirectToAction("ManageLogins", new {Message = message});
-        }
-
-        //
-        // GET: /Manage/AddPhoneNumber
-        public ActionResult AddPhoneNumber()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Manage/AddPhoneNumber
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> AddPhoneNumber(AddPhoneNumberViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-            // Token generieren und senden
-            // Change PrimaryKey of identity package to int [var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), model.Number);] //KrabsJ
-            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId<int>(),
-                model.Number);
-            if (UserManager.SmsService != null)
-            {
-                var message = new IdentityMessage
-                {
-                    Destination = model.Number,
-                    Body = "Ihr Sicherheitscode lautet " + code
-                };
-                await UserManager.SmsService.SendAsync(message);
-            }
-            return RedirectToAction("VerifyPhoneNumber", new {PhoneNumber = model.Number});
-        }
-
-        //
-        // POST: /Manage/EnableTwoFactorAuthentication
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> EnableTwoFactorAuthentication()
-        {
-            /* Change PrimaryKey of identity package to int //KrabsJ
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), true);
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            */
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId<int>(), true);
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-            if (user != null)
-                await SignInManager.SignInAsync(user, false, false);
-            return RedirectToAction("Index", "Manage");
-        }
-
-        //
-        // POST: /Manage/DisableTwoFactorAuthentication
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DisableTwoFactorAuthentication()
-        {
-            /*Change PrimaryKey of identity package to int //KrabsJ
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), false);
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            */
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId<int>(), false);
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-            if (user != null)
-                await SignInManager.SignInAsync(user, false, false);
-            return RedirectToAction("Index", "Manage");
-        }
-
-        //
-        // GET: /Manage/VerifyPhoneNumber
-        public async Task<ActionResult> VerifyPhoneNumber(string phoneNumber)
-        {
-            // Change PrimaryKey of identity package to int [var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId(), phoneNumber);] //KrabsJ
-            var code = await UserManager.GenerateChangePhoneNumberTokenAsync(User.Identity.GetUserId<int>(),
-                phoneNumber);
-            // Eine SMS über den SMS-Anbieter senden, um die Telefonnummer zu überprüfen.
-            return phoneNumber == null
-                ? View("Error")
-                : View(new VerifyPhoneNumberViewModel {PhoneNumber = phoneNumber});
-        }
-
-        //
-        // POST: /Manage/VerifyPhoneNumber
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> VerifyPhoneNumber(VerifyPhoneNumberViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-            // Change PrimaryKey of identity package to int [var result = await UserManager.ChangePhoneNumberAsync(User.Identity.GetUserId(), model.PhoneNumber, model.Code);] //KrabsJ
-            var result =
-                await UserManager.ChangePhoneNumberAsync(User.Identity.GetUserId<int>(), model.PhoneNumber, model.Code);
-            if (result.Succeeded)
-            {
-                //Change PrimaryKey of identity package to int [var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());] //KrabsJ
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-                if (user != null)
-                    await SignInManager.SignInAsync(user, false, false);
-                return RedirectToAction("Index", new {Message = ManageMessageId.AddPhoneSuccess});
-            }
-            // Wurde dieser Punkt erreicht, ist ein Fehler aufgetreten. Formular erneut anzeigen.
-            ModelState.AddModelError("", "Fehler beim Überprüfen des Telefons.");
-            return View(model);
-        }
-
-        //
-        // POST: /Manage/RemovePhoneNumber
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RemovePhoneNumber()
-        {
-            // Change PrimaryKey of identity package to int [var result = await UserManager.SetPhoneNumberAsync(User.Identity.GetUserId(), null);] //KrabsJ
-            var result = await UserManager.SetPhoneNumberAsync(User.Identity.GetUserId<int>(), null);
-            if (!result.Succeeded)
-                return RedirectToAction("Index", new {Message = ManageMessageId.Error});
-            // Change PrimaryKey of identity package to int [var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());] //KrabsJ
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-            if (user != null)
-                await SignInManager.SignInAsync(user, false, false);
-            return RedirectToAction("Index", new {Message = ManageMessageId.RemovePhoneSuccess});
-        }
-
-        //
         // GET: /Manage/ChangePassword
         public ActionResult ChangePassword()
         {
             return View();
         }
 
-        //
         // POST: /Manage/ChangePassword
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -269,89 +85,6 @@ namespace BiberDAMM.Controllers
             return View(model);
         }
 
-        //
-        // GET: /Manage/SetPassword
-        public ActionResult SetPassword()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Manage/SetPassword
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> SetPassword(SetPasswordViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Change PrimaryKey of identity package to int [var result = await UserManager.AddPasswordAsync(User.Identity.GetUserId(), model.NewPassword);] //KrabsJ
-                var result = await UserManager.AddPasswordAsync(User.Identity.GetUserId<int>(), model.NewPassword);
-                if (result.Succeeded)
-                {
-                    // Change PrimaryKey of identity package to int [var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());] //KrabsJ
-                    var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-                    if (user != null)
-                        await SignInManager.SignInAsync(user, false, false);
-                    return RedirectToAction("Index", new {Message = ManageMessageId.SetPasswordSuccess});
-                }
-                AddErrors(result);
-            }
-
-            // Wurde dieser Punkt erreicht, ist ein Fehler aufgetreten. Formular erneut anzeigen.
-            return View(model);
-        }
-
-        //
-        // GET: /Manage/ManageLogins
-        public async Task<ActionResult> ManageLogins(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.RemoveLoginSuccess
-                    ? "Die externe Anmeldung wurde entfernt."
-                    : message == ManageMessageId.Error
-                        ? "Fehler"
-                        : "";
-            // Change PrimaryKey of identity package to int [var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());] //KrabsJ
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId<int>());
-            if (user == null)
-                return View("Error");
-            // Change PrimaryKey of identity package to int [var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());] //KrabsJ
-            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId<int>());
-            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes()
-                .Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
-            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
-            return View(new ManageLoginsViewModel
-            {
-                CurrentLogins = userLogins,
-                OtherLogins = otherLogins
-            });
-        }
-
-        //
-        // POST: /Manage/LinkLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult LinkLogin(string provider)
-        {
-            // Umleitung an den externen Anmeldeanbieter anfordern, um eine Anmeldung für den aktuellen Benutzer zu verknüpfen.
-            return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"),
-                User.Identity.GetUserId());
-        }
-
-        //
-        // GET: /Manage/LinkLoginCallback
-        public async Task<ActionResult> LinkLoginCallback()
-        {
-            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
-            if (loginInfo == null)
-                return RedirectToAction("ManageLogins", new {Message = ManageMessageId.Error});
-            // Change PrimaryKey of identity package to int [var result = await UserManager.AddLoginAsync(User.Identity.GetUserId(), loginInfo.Login);] //KrabsJ
-            var result = await UserManager.AddLoginAsync(User.Identity.GetUserId<int>(), loginInfo.Login);
-            return result.Succeeded
-                ? RedirectToAction("ManageLogins")
-                : RedirectToAction("ManageLogins", new {Message = ManageMessageId.Error});
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing && _userManager != null)
@@ -365,46 +98,11 @@ namespace BiberDAMM.Controllers
 
         #region Hilfsprogramme
 
-        // Wird für XSRF-Schutz beim Hinzufügen externer Anmeldungen verwendet.
-        private const string XsrfKey = "XsrfId";
-
-        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
-
         private void AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
                 ModelState.AddModelError("", error);
         }
-
-        private bool HasPassword()
-        {
-            // Change PrimaryKey of identity package to int [var user = UserManager.FindById(User.Identity.GetUserId());] //KrabsJ
-            var user = UserManager.FindById(User.Identity.GetUserId<int>());
-            if (user != null)
-                return user.PasswordHash != null;
-            return false;
-        }
-
-        private bool HasPhoneNumber()
-        {
-            // Change PrimaryKey of identity package to int [var user = UserManager.FindById(User.Identity.GetUserId());] //KrabsJ
-            var user = UserManager.FindById(User.Identity.GetUserId<int>());
-            if (user != null)
-                return user.PhoneNumber != null;
-            return false;
-        }
-
-        public enum ManageMessageId
-        {
-            AddPhoneSuccess,
-            ChangePasswordSuccess,
-            SetTwoFactorSuccess,
-            SetPasswordSuccess,
-            RemoveLoginSuccess,
-            RemovePhoneSuccess,
-            Error
-        }
-
         #endregion
     }
 }
